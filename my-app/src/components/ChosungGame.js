25ded32774967dd1288f9ce03a876acb3edb1f6b
수박이 <102145617+subak222@users.noreply.github.com>
2023-06-29 08:18:41 +0900

    css + firebase

--- my-app/src/components/ChosungGame.js
+++ my-app/src/components/ChosungGame.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import data from './data';
+import './ChosungGame.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const ChosungGame = () => {
   const [gameStarted, setGameStarted] = useState(false);
@@ -8,7 +11,7 @@ const ChosungGame = () => {
   const [chosung, setChosung] = useState('');
   const [hint, setHint] = useState('');
   const [score, setScore] = useState(0);
-  const [highScore, setHighScore] = useState(0);
+  const [highScore, setHighScore] = useState();
   const [answerResult, setAnswerResult] = useState('');
   const [inputValue, setInputValue] = useState('');
   const [time, setTime] = useState(10);
@@ -63,27 +66,39 @@ const ChosungGame = () => {
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && time > 0 && !inputRef.current.hasAttribute('readonly')) {
       clearTimeout(timerRef.current);
-
+  
       if (inputValue === songData.answer) {
         setScore((prevScore) => prevScore + 1);
         setAnswerResult('correct');
         setInputValue('');
         setDelay(3000);
+        inputRef.current.setAttribute('readonly', 'readonly'); // 입력창을 readonly 상태로 변경
         timerRef.current = setTimeout(() => {
           generateChosung();
           setTime(10);
           setAnswerResult('');
+          inputRef.current.removeAttribute('readonly'); // 입력창의 readonly 속성 제거
+          if (inputRef.current) {
+            inputRef.current.focus();
+          }
         }, 3000);
       } else {
         setAnswerResult('wrong');
         setInputValue('');
         setDelay(3000);
+        inputRef.current.setAttribute('readonly', 'readonly'); // 입력창을 readonly 상태로 변경
         timerRef.current = setTimeout(() => {
           setGameOver(true);
         }, 3000);
       }
+    }
+  };
+  
+  const handleInputFocus = () => {
+    if (!inputRef.current.hasAttribute('readonly')) {
+      inputRef.current.focus();
     }
   };
 
@@ -99,46 +114,70 @@ const ChosungGame = () => {
   }, [gameStarted]);
 
   useEffect(() => {
+    const db = firebase.firestore();
     if (score > highScore) {
       setHighScore(score);
+      db.collection('scoreboard').doc("scores").update({"bestscore": score});
     }
   }, [score]);
 
+  useEffect(()=> {
+    const db = firebase.firestore();
+    db.collection('scoreboard').get().then((result) =>{
+      result.forEach((doc)=>{
+        setHighScore(Object.values(doc.data())[0]);
+      })
+    })
+  })
+
   return (
-    <div>
-      <h1>Chosung Game</h1>
+    <div className="container">
+      <h1 className="title">샤이니 노래 제목 초성 퀴즈</h1>
       {!gameStarted && !gameOver && (
-        <button onClick={startGame}>Start Game</button>
+        <button className="start-button" onClick={startGame}>
+          게임 시작
+        </button>
       )}
       {gameStarted && !gameOver && (
         <>
           <div>
-            <p>Chosung: {chosung}</p>
-            <p>Hint: {hint}</p>
-            <p>Time: {time} sec</p>
+            <p className="score">내 점수: {score}</p>
+          </div>
+          <div>
+            <p>초성: {chosung}</p>
+            <p>힌트: {hint}</p>
+            <p>남은 시간: {time} sec</p>
             <input
               ref={inputRef}
               type="text"
               value={inputValue}
               onChange={handleChange}
               onKeyDown={handleKeyDown}
+              onFocus={handleInputFocus}
             />
             {answerResult === 'correct' && <p>정답입니다!</p>}
             {answerResult === 'wrong' && (
               <p>틀렸습니다. 정답은 {songData.answer}입니다.</p>
             )}
           </div>
-          <div>
-            <p>Score: {score}</p>
-          </div>
         </>
       )}
       {gameOver && (
         <div>
-          <p>Game Over!</p>
-          <p>Your Score: {score}</p>
-          <p>High Score: {highScore}</p>
-          <button onClick={handleTryAgain}>Try Again</button>
+          <p className='endgame'>게임 종료!</p>
+          <div className='bigbox'>
+            <div className="box">
+              <p>내 점수</p>
+              <p>{score}</p>
+            </div>
+            <div className="box">
+              <p>최고 기록</p>
+              <p>{highScore}</p>
+            </div>
+          </div>
+          <button className="try-again-button" onClick={handleTryAgain}>
+            재시작
+          </button>
         </div>
       )}
     </div>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,28 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5y5xblS3TmxyT_Fp80HEpQgJ0GEoGJIk",
+  authDomain: "quiz-4759b.firebaseapp.com",
+  projectId: "quiz-4759b",
+  storageBucket: "quiz-4759b.appspot.com",
+  messagingSenderId: "623401422869",
+  appId: "1:623401422869:web:35abf181a138b2982cfe1c",
+  measurementId: "G-PJK526MR4S"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore;
+
 root.render(
   <StrictMode>
     <App />
